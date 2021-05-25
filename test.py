a99fd230a990df5673132bc240e379f92683ebc8
Timezonedesign <bolesalavb@gmail.com>
2021-05-26 04:22:09 +0900

    fix issue

--- test.py
+++ test.py
@@ -15,19 +15,26 @@ for i in [100,101,102,103,104,105,106,107,108,109,110,111,112,113,115]:
 		continue
 
 	text = soup.select('div.cat-info-txt a')[0].get_text()
-	cat_baner_links = soup.select('div.cat-banner_s li a')
-	cat_baner_images = soup.select('div.cat-banner_s li a img')
-
+	item_links = soup.select('ul.cat-banner_s li a')
+	item_images = soup.select('ul.cat-banner_s li a img')
+	items = []
+	for j in range(len(item_links)):
+		item = {
+			'link': "/shop_menu/set/cat/"+item_links[j]['href'],
+			'image': item_images[j]['src'].replace('../..', '/assets/img')
+		}
+		items.append(item)
 	row = {
 		'url': "shop_menu/set/cat/"+str(i)+".html",
 		'img': img.replace('../..', '/assets/img'),
-		'text': text
+		'text': text,
+		'items': json.dumps(items)
 	}
 	data.append(row)
 
 with open("data.csv", 'w', encoding="UTF-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=list(row.keys()))
     writer.writeheader()
-    for datum in data:
-    	writer.writerow({'url': datum['url'], 'img': datum['img'], 'text':datum['text']})
+    for row in data:
+    	writer.writerow(row)
 
