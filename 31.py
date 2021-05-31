2faa9cce48c2b46920379706100a75e1034a73f6
Timezonedesign <bolesalavb@gmail.com>
2021-05-31 10:09:54 +0900

    fix some issues

--- 31.py
+++ 31.py
@@ -12,11 +12,11 @@ def getValue(element, attribute = '', str1 = '', str2 = ''):
 		return ''
 
 root = "https://www.kts-web.com/"
-path = "tire_wheel/tire/"
+path = "product/"
 item_path = "shop_menu/set"
 data = []
-for i in ['bridgestone','dunlop','falken','federal','goodyear','kenda','michelin','nankang','toyo_tires','yokohama']:
-	page = requests.get(root + path + str(i) + "/index.html")
+for i in ['body','brake','cooling','drive','engine','intake','interior','other','suspension','ex']:
+	page = requests.get(root + path + str(i) + '/index.html')
 	soup = BeautifulSoup(page.content, 'html.parser')
 
 	meta_refresh = getValue(soup.find('meta', attrs = {'http-equiv':'refresh'}), 'content', '0; url=https://www.kts-web.com','')
@@ -24,34 +24,46 @@ for i in ['bridgestone','dunlop','falken','federal','goodyear','kenda','michelin
 	meta_description = getValue(soup.find('meta', attrs = {'name':'description'}), 'content')
 	meta_keywords = getValue(soup.find('meta', attrs = {'name':'keywords'}), 'content')
 
-	img = '/assets/img/tire_wheel/tire/' + i + '/' + getValue(soup.select('img.sp_img_100p'), 'src')
-	img_alt = getValue(soup.select('img.sp_img_100p'), 'alt')
-	text = ''
-	item_links = soup.select('div.th_maker_top_list_cell2 a.link_n')
-	item_images = soup.select('div.th_maker_top_list_cell2 img')
+	section_titles = soup.select('div.pr_title_2')
+	section_item_groups = soup.select('div.pr_title_pc')
 	items = []
-	for j in range(len(item_links)):
-		item = {
-			'link': path + str(i) + "/" + getValue(item_links[j], 'href'),
-			'image': '/assets/img/tire_wheel/tire/' + i + '/' + getValue(item_images[j], 'src'),
-			'image_alt': getValue(item_images[j], 'alt'),
-			'text': getValue(item_links[j])
-		}
-		items.append(item)
+	if(len(section_titles) == 0 and len(section_item_groups) > 0):
+		section_titles = ['']
+	for j in range(len(section_titles)):
+		try:
+			section_item_links = section_item_groups[j].select('a')
+			section_item_images = section_item_groups[j].select('a img')
+		except:
+			section_item_links = soup.select('div.bq_title a')
+			section_item_images = soup.select('div.bq_title a img')
+		# print(section['title'])
+		# print(section_items)
+		# break;
+		section_items = []
+		for k in range(len(section_item_links)):
+			item = {
+				'link': "/" + path + str(i) + "/" + getValue(section_item_links[k], 'href'),
+				'image': '/assets/img/product/' + str(i) + '/' + getValue(section_item_images[k], 'src'),
+				'image_alt': getValue(section_item_images[k], 'alt')
+			}
+			section_items.append(item)
+
+		items.append({
+				'title': getValue(section_titles[j]),
+				'items': section_items
+			})
+
 	row = {
 		'url': path + str(i) + "/index.html",
 		'meta_refresh': meta_refresh,
 		'meta_title': meta_title,
 		'meta_description': meta_description,
 		'meta_keywords': meta_keywords,
-		'img': img,
-		'img_alt': img_alt,
-		'text': text,
 		'items': json.dumps(items, ensure_ascii=False)
 	}
 	data.append(row)
 
-with open("42.csv", 'w', encoding="UTF-8", newline="") as f:
+with open("31.csv", 'w', encoding="UTF-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=list(row.keys()))
     writer.writeheader()
     for row in data:
